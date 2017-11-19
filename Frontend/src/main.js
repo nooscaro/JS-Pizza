d6385164df11978e2c635f5e5681cc005ad40bc5
Vera Baklanova <vera.baklanova@github.com>
2017-11-19 20:51:13 +0200

    tackled the pizza marker

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -66,17 +66,19 @@ function	initialize() {
 
     var mapProp = {
         center: new google.maps.LatLng(50.464379, 30.519131),
-        zoom: 11
+        zoom: 15
     };
     var html_element = document.getElementById("map");
     var map = new google.maps.Map(html_element, mapProp);
+    // ІКОНКА ПІЦИ НА КАРТІ
+    var point	=	new	google.maps.LatLng(50.464379,30.519131);
 
     // var point	=	new	google.maps.LatLng(50.464379,30.519131);
-    // var marker	=	new	google.maps.Marker({
-    //     position:	point,
-    //     map:	map,
-    //     icon:	"assets/images/map-icon.png"
-    // });
+    var marker	=	new	google.maps.Marker({
+        position:	point,
+        map:	map,
+        icon:	"assets/images/map-icon.png"
+    });
 }
 //Карта створена і показана
 
